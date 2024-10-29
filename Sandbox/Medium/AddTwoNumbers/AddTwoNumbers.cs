2f1a7ef751d9f611663e6001b8b625d8646e2ef0
karimmoustamid <moustamid.teck@gmail.com>
2024-10-29 08:51:11 -0600

    Add ListNode definition and add .idea layout file
    Included the definition for singly-linked list (ListNode) within AddTwoNumbers.cs. Added .idea/indexLayout.xml to version control for project configuration management.

--- Sandbox/Medium/AddTwoNumbers/AddTwoNumbers.cs
+++ Sandbox/Medium/AddTwoNumbers/AddTwoNumbers.cs
@@ -1,5 +1,15 @@
 namespace Sandbox.Medium.AddTwoNumbers
 {
+    /**
+    * Definition for singly-linked list.
+    * struct ListNode {
+    *     int val;
+    *     ListNode *next;
+    *     ListNode() : val(0), next(nullptr) {}
+    *     ListNode(int x) : val(x), next(nullptr) {}
+    *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    * };
+     */
     public class AddTwoNumbers
     {
     }
