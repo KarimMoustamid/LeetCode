85e30c8ec4cfda8f53d6d57ad63d26ccebc42dca
karimmoustamid <karim.moustamid@agilethought.com>
2024-11-18 09:48:43 -0600

    Add FirstMissingPositiveSolution class and update Program
    Introduce the `FirstMissingPositiveSolution` class in a new file to address finding the first missing positive in an array. Update `Program.cs` to instantiate and reference this new solution class.

---
+++ Top_Interview_Questions/Array&Strings/First Missing Positive/FirstMissingPositiveSolution.cs
@@ -0,0 +1,11 @@
+namespace Top_Interview_Questions.Array_Strings.First_Missing_Positive
+{
+    public class FirstMissingPositiveSolution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+
+            return 0;
+        }
+    }
+}
--- Top_Interview_Questions/Program.cs
+++ Top_Interview_Questions/Program.cs
@@ -1,16 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Top_Interview_Questions.Array_Strings.Game_of_Life;
+﻿using Top_Interview_Questions.Array_Strings.First_Missing_Positive;
 
-int m = 3;
-int n = 3;
 
-int[][] board =
-{
-    new int[] {1, 2, 3},
-    new int[] {4, 5, 6},
-    new int[] {7, 8, 9}
-};
 
-GameOfLife solution = new GameOfLife();
-
-solution.NextStage(board);
+FirstMissingPositiveSolution solution = new FirstMissingPositiveSolution();
