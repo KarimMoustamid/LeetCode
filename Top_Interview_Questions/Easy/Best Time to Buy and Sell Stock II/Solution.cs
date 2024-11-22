93e09741651b4b0b595403b40970017ffb4b6df3
karimmoustamid <moustamid.teck@gmail.com>
2024-11-21 21:46:16 -0600

    Add Buy and Sell Stock II solution and update README
    Introduced the `Best Time to Buy and Sell Stock II` solution in the Easy category and updated the corresponding README.md with examples and constraints. Refactored Program.cs to integrate and test the newly added solution.

---
+++ Top_Interview_Questions/Easy/Best Time to Buy and Sell Stock II/Solution.cs
@@ -0,0 +1,10 @@
+namespace Top_Interview_Questions.Easy.Best_Time_to_Buy_and_Sell_Stock_II
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            return 0;
+        }
+    }
+}
--- Top_Interview_Questions/Program.cs
+++ Top_Interview_Questions/Program.cs
@@ -1,9 +1,6 @@
-﻿using Top_Interview_Questions.Easy.Remove_Duplicates_from_Sorted_Array;
+﻿using Top_Interview_Questions.Easy.Best_Time_to_Buy_and_Sell_Stock_II;
 
-int[] nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+int[] prices = { 100, 98, 102, 95, 110, 120, 105 };
 
 Solution solution = new Solution();
-int k = solution.RemoveDuplicates(nums);
-Console.BackgroundColor = ConsoleColor.Green;
-Console.ForegroundColor = ConsoleColor.White;
-Console.WriteLine($"the number of unique elements in nums is : {k}");
+int maxProfit = solution.MaxProfit(prices);
