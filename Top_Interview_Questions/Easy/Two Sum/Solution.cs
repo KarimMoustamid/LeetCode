e187744fc5fd60d2d3959ec23d38a7bea48d1da3
karimmoustamid <karim.moustamid@agilethought.com>
2024-11-27 20:52:49 -0600

    Add initial Two Sum problem setup
    Created the README with problem details, examples, and constraints for the Two Sum problem. Provided a Solution.cs file as a placeholder for the upcoming solution implementation and modified Program.cs to initialize sample input for the Two Sum problem.

---
+++ Top_Interview_Questions/Easy/Two Sum/Solution.cs
@@ -0,0 +1,7 @@
+namespace Top_Interview_Questions.Easy.Two_Sum
+{
+    public static class Solution
+    {
+
+    }
+}
--- Top_Interview_Questions/Program.cs
+++ Top_Interview_Questions/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Top_Interview_Questions.Easy.Move_Zeroes;
 
-int[] nums = [0, 1, 0, 3, 12];
-
-Solution.MoveZeroes(nums);
+int[] nums = [2, 7, 11, 15];
+int target = 9;
