bcebaac021a4b053cc3e833a20e8d84766d78be9
karimmoustamid <moustamid.teck@gmail.com>
2024-12-15 13:42:19 -0600

    Update project metadata and configuration files
    Updated auto-generated assembly information, NuGet configurations, and IDE-specific files across multiple projects. These changes ensure compatibility with the latest development tools and provide consistent build metadata throughout the codebase. Adjusted NuGet tool version and added a minor property for clarity.

--- LeetCode/obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
+++ LeetCode/obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b304ed579cebdf28ea56f4b66a01362301071f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06973c8359c36af39717bdcc67b537cbfe7b6ffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
